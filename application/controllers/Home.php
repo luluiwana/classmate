f5e3cd74bc034597a99111fd05ed26488aff7185
Lulu Iwana <lulukiwanatul@gmail.com>
2021-08-15 19:11:48 +0700

    new template

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,16 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->view('home/header');
+		$this->load->view('home/login');
+		$this->load->view('home/footer');
 		
+	}
+	public function daftar()
+	{
+		$this->load->view('home/header');
+		$this->load->view('home/daftar');
+		$this->load->view('home/footer');
+
 	}
 }
